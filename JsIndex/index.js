cc9ed088429140adeab53a937f5b5348d9d7aa1d
fanxuewen <1020977852@qq.com>
2019-04-12 15:32:31 +0800

    index

---
+++ JsIndex/index.js
@@ -0,0 +1,62 @@
+// ***********find******************//
+
+
+var findArr = [1, 2, 3, 4];
+var res1 = findArr.find(item => {
+    return item > 2;
+})
+var res2 = findArr.find(item => {
+    return item > 10;
+})
+
+console.log('res1', res1);
+console.log('res2', res2);
+
+
+// ***********find******************//
+
+// var findArr1 = [1, , 3];
+// console.log('--------find---------');
+// var res1 = findArr1.find((item, index) => {
+//     console.log(item, '----', index)
+// })
+// console.log('--------map---------');
+
+// findArr1.map((item, index) => {
+//     console.log(item, '----', index);
+// })
+
+
+
+// ***********findIndex******************//
+// var findIndexArr = [1, 2, 3, 4];
+// var res1 = findIndexArr.findIndex(item => {
+//     return item > 2;
+// })
+// var res2 = findIndexArr.findIndex(item => {
+//     return item > 10;
+// })
+// console.log('res1', res1);
+// console.log('res2', res2);
+
+
+// ***********IndexOf******************//
+// var indexOfArr = [1, 2, 3, 4];
+// indexOfArr.indexOf(2)  //1
+// indexOfArr.indexOf(2,2)//-1
+// indexOfArr.indexOf(9)  //-1
+
+// ***********includes******************//
+// var includesArr = [1, 2, 3, 4];
+//     includesArr.indexOf(2)  //true
+//     includesArr.indexOf(9)  //false
+
+
+
+
+
+
+
+
+
+

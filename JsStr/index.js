51fd737ec501ed081a0e405812ee371bc4114e2c
fanxuewen <1020977852@qq.com>
2019-04-13 19:42:34 +0800

    jsstr

---
+++ JsStr/index.js
@@ -0,0 +1,21 @@
+// let str='好好学习，天天向上';
+// console.log('substr---:',str.substr(2));
+// console.log('substring:',str.substring(2));
+// console.log('slice----:',str.slice(2));
+
+
+
+// let str='好好学习，天天向上';
+// console.log('substr---:',str.substr(-2));
+// console.log('substring:',str.substring(-2));
+// console.log('slice----:',str.slice(-2));
+
+// let str='好好学习，天天向上';
+// console.log('substr---:',str.substr(2,4));
+// console.log('substring:',str.substring(2,4));
+// console.log('slice----:',str.slice(2,4));
+
+let str='好好学习，天天向上';
+console.log('substr---:',str.substr(2,1));
+console.log('substring:',str.substring(2,1));
+console.log('slice----:',str.slice(2,1));
